8d39d19bfdb7e34a4d0ad3a5e2e2f49e3c543529
haroldo <haroldoop@gmail.com>
2019-11-18 21:50:41 -0300

    Added a textarea, just for testing.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      value: 'This is another test'
+    };
+    
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +28,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <textarea value={this.state.value} onChange={this.handleChange} />
       </div>
     );
   }
