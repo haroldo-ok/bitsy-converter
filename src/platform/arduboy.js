25a7bde13c12b131e8987bda00c47f3ebdf26c31
haroldo <haroldoop@gmail.com>
2019-12-18 23:05:01 -0300

    Generate all frames plus an enum for image indexes

--- src/platform/arduboy.js
+++ src/platform/arduboy.js
@@ -5,10 +5,29 @@ const transpose = a => a[0].map((_, c) => a.map(r => r[c]));
 const toBinaryConst = a => `B${a.join('')}`;
 const convertTile = tile => transpose(tile).map(row => toBinaryConst(row.reverse())).join(', ');
 
+const toEnumDeclaration = (name, object) =>`
+enum ${name} {
+${ Object.entries(object).map(([k, i]) => `  ${k} = ${i}`).join(',\n') }
+};`
+
+const toTileFrames = images => `{
+${ Object.entries(images).map(([k, v]) => {
+  const frames = v.map(convertTile);
+  return  `  // ${k}
+  { ${ frames.map(f => `{ ${f} }`).join('\n  ') } }`;
+}).join('\n') }
+}`
+
+const toImageDeclaration = (name, frames) => `
+const uint8_t PROGMEM ${name}[${frames.length}][8] = { ${ frames.map(f => `{ ${convertTile(f)} }`) } };
+`.trim();
+
 export const convertArduboy = code => {
   const world = parseWorld(code);
-  return Object.entries(world.images).map(([k, v]) => {
-    const frames = v.map(convertTile);
-    return  `const uint8_t PROGMEM img_${k}[] = { ${frames.join('\n')} };`;
-  }).join('\n');
+  const imageIndexes = Object.fromEntries(Object.keys(world.images).map((k, i) => [k, i]));
+
+  return [
+	  toEnumDeclaration('ImageIndex', imageIndexes),
+	  ...Object.entries(world.images).map(([k, v]) => toImageDeclaration(`img_${k}`, v)),
+  ].join('\n\n');
 }
