31936a9872252e88b3aad708b0878047969e72df
haroldo <haroldoop@gmail.com>
2019-12-18 21:37:16 -0300

    Code cleanup on C array generation

--- src/platform/arduboy.js
+++ src/platform/arduboy.js
@@ -2,12 +2,13 @@ import {parseWorld} from 'bitsy-parser';
 
 const transpose = a => a[0].map((_, c) => a.map(r => r[c]));
 
+const toBinaryConst = a => `B${a.join('')}`;
+const convertTile = tile => transpose(tile).map(row => toBinaryConst(row.reverse())).join(', ');
+
 export const convertArduboy = code => {
   const world = parseWorld(code);
   return Object.entries(world.images).map(([k, v]) => {
-    const frames = v.map(frame => {
-      return transpose(frame).map(row => 'B' + row.reverse().join('')).join(', ');
-    });
+    const frames = v.map(convertTile);
     return  `const uint8_t PROGMEM img_${k}[] = { ${frames.join('\n')} };`;
   }).join('\n');
 }
