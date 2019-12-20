3d879a4d5408a16ab371a398a3696c0528ae31a8
haroldo <haroldoop@gmail.com>
2019-12-19 22:40:34 -0300

    Image generation modified to use offsets

--- src/platform/arduboy.js
+++ src/platform/arduboy.js
@@ -5,29 +5,41 @@ const transpose = a => a[0].map((_, c) => a.map(r => r[c]));
 const toBinaryConst = a => `B${a.join('')}`;
 const convertTile = tile => transpose(tile).map(row => toBinaryConst(row.reverse())).join(', ');
 
-const toEnumDeclaration = (name, object) =>`
+const toEnumDeclaration = (name, object, keyFunction = k => k) =>`
 enum ${name} {
-${ Object.entries(object).map(([k, i]) => `  ${k} = ${i}`).join(',\n') }
+${ Object.entries(object).map(([k, i]) => `  ${keyFunction(k)} = ${i}`).join(',\n') }
 };`
 
-const toTileFrames = images => `{
-${ Object.entries(images).map(([k, v]) => {
-  const frames = v.map(convertTile);
-  return  `  // ${k}
-  { ${ frames.map(f => `{ ${f} }`).join('\n  ') } }`;
-}).join('\n') }
-}`
+const toImageDeclaration = (name, imageInfos) => {
+  const content = imageInfos.map(({name, frames, index, offset}) => `
+  // ${name}: index ${index}, offset ${offset}, ${frames.length} frame(s)
+  ${frames.map(frame => `{ ${convertTile(frame)} }`).join(',\n  ')}`).join(',');
+  
+  return `const uint8_t PROGMEM ${name}[][8] = { 
+${content} 
+};`
+}
 
-const toImageDeclaration = (name, frames) => `
-const uint8_t PROGMEM ${name}[${frames.length}][8] = { ${ frames.map(f => `{ ${convertTile(f)} }`) } };
-`.trim();
+const extractImageInfos = world => {
+  const withBlank = [ ['BLANK', [ Array(8).fill(Array(8).fill(0)) ] ], ...Object.entries(world.images) ];
+  const imageInfos = withBlank.map(([name, frames], index) => ({ name, frames, index }));
+  
+  const withOffsets = imageInfos.reduce(({offset, results}, info) => ({
+    offset: offset + info.frames.length,
+    results: [...results, {offset, ...info}]
+  }), {offset: 0, results: []}).results;  
+  
+  return withOffsets;
+}
 
 export const convertArduboy = code => {
   const world = parseWorld(code);
-  const imageIndexes = Object.fromEntries(Object.keys(world.images).map((k, i) => [k, i]));
+  const imageInfos = extractImageInfos(world);
+  
+  const imageIndexes = Object.fromEntries(imageInfos.map(({name, offset}) => [name, offset]));
 
   return [
-	  toEnumDeclaration('ImageIndex', imageIndexes),
-	  ...Object.entries(world.images).map(([k, v]) => toImageDeclaration(`img_${k}`, v)),
+	  toEnumDeclaration('ImageOffsets', imageIndexes, k => `ofs_${k}`),
+	  toImageDeclaration('images', imageInfos),
   ].join('\n\n');
 }
