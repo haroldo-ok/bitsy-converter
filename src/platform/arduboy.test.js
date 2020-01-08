2a074c4e4fb74a130a81f00007f89078e4493136
haroldo <haroldoop@gmail.com>
2020-01-07 23:23:48 -0300

    Create unit test based on preparsed JSON

--- src/platform/arduboy.test.js
+++ src/platform/arduboy.test.js
@@ -1,9 +1,20 @@
 import fs from 'promise-fs';
 
+import worldObject from './example.json';
+
 import {convertArduboy} from './arduboy';
 
 it('converts Bitsy script with no error', async () => {
 	const source = String(await fs.readFile('./src/example.bitsy'));
+	expect(source).toMatch("Your game's title here");
+
 	const generatedCode = convertArduboy(source);
 	expect(generatedCode).toMatch("Your game's title here");
+});
+
+it('converts a preparsed Bitsy object with no error', async () => {
+	expect(worldObject).toBeTruthy();	
+	expect(worldObject).toMatchObject({
+		title: "Your game's title here"
+	});	
 });
